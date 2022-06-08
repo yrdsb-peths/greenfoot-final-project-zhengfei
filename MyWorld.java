3c8a4481a3b3a4af3a20cd73344684ec954cea81
ZhengfeizZZ <349565531@gapps.yrdsb.ca>
2022-06-08 11:24:43 -0400

    Added a Key class

--- MyWorld.java
+++ MyWorld.java
@@ -16,10 +16,10 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1, false); 
-        
+        super(1000, 700, 1, false); 
+
         // Creates the miku object.
         Miku miku = new Miku();
-        addObject(miku, 300, 300);
+        addObject(miku, 500, 350);
     }
 }
