97d3c287667fc77db85745b106aa393907910fc9
ZhengfeizZZ <349565531@gapps.yrdsb.ca>
2022-06-08 14:21:31 -0400

    Created Miku constructor and added Miku's animation images

--- Miku.java
+++ Miku.java
@@ -8,14 +8,35 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Miku extends Actor
 {
-    GreenfootSound backgroundSound = new GreenfootSound("World_is_Mine-Hatsune_Miku.mp3");
+
     /**
      * Act - do whatever the Miku wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    GreenfootSound backgroundSound = new GreenfootSound("World_is_Mine-Hatsune_Miku.mp3");
+    GreenfootImage[] image;
+    
+    /**
+     * Constructor: Runs everytime when an object is created.
+     */
+    
+    public Miku()
+    {
+        image = new GreenfootImage[15];
+        for(int i = 0; i < image.length; i++)
+        {
+            image[i] = new GreenfootImage("Images/AnimateMiku/AnimateMiku" + i + ".png");
+            image[i].scale(200, 150);
+        }
+        
+        // Initial Miku image.
+        setImage(image[0]);
+    }
+    
     public void act()
     {
         // Add your action code here.
+        
         //plays the background song.
         backgroundSound.play();
     }
