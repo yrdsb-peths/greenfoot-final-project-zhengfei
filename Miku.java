ba89c77e54d41408b934df43427b9571f77a5563
ZhengfeizZZ <349565531@gapps.yrdsb.ca>
2022-06-08 14:08:49 -0400

    Added background sound when game starts

--- Miku.java
+++ Miku.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Miku extends Actor
 {
+    GreenfootSound backgroundSound = new GreenfootSound("World_is_Mine-Hatsune_Miku.mp3");
     /**
      * Act - do whatever the Miku wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +16,7 @@ public class Miku extends Actor
     public void act()
     {
         // Add your action code here.
+        //plays the background song.
+        backgroundSound.play();
     }
 }
