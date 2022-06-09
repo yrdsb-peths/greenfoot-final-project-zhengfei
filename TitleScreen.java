c48ebf2a1ce2c090ae32286ddb90cebd240c37d5
ZhengfeizZZ <349565531@gapps.yrdsb.ca>
2022-06-09 13:50:48 -0400

    Added background image for title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -17,9 +17,10 @@ public class TitleScreen extends World
     public TitleScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1000, 700, 1); 
     }
     
+    // Background music is played when user hits run.
     public void act()
     {
         // Add your action code here.
