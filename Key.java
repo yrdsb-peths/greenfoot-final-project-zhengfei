65dc457dfaa66c2c78daa8ff259f6e5e3f7cf4b5
ZhengfeizZZ <349565531@gapps.yrdsb.ca>
2022-06-13 13:58:39 -0400

    Add key falling down

--- Key.java
+++ Key.java
@@ -12,8 +12,15 @@ public class Key extends Actor
      * Act - do whatever the Key wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int speed = 1;
+    
     public void act()
     {
         // Add your action code here.
+        
+        // Key falls down.
+        int x = getX();
+        int y = getY() + speed;
+        setLocation(x, y);
     }
 }
